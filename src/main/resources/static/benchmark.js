595835f4010a4a3c967f4c12a23fc3139706ff7e
NEMRI, Aymen <aymen.nemri@ge.com>
2017-07-24 14:45:17 +0200

    add XHR and Angular decorator

--- src/main/resources/static/benchmark.js
+++ src/main/resources/static/benchmark.js
@@ -12,6 +12,21 @@ app.config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
 }]);
 
+/*********************************Decorator ***************************************/
+app.decorator("$xhrFactory", [
+  "$delegate", "$injector",
+  function($delegate, $injector) {
+    return function(method, url) {
+      var xhr = $delegate(method, url);
+      var $http = $injector.get("$http");
+      var callConfig = $http.pendingRequests[$http.pendingRequests.length - 1];
+      if (angular.isFunction(callConfig.onProgress))
+        xhr.addEventListener("progress", callConfig.onProgress);
+      return xhr;
+    };
+  }
+]);
+
 app.service('simpleRequestService', function ($http) {
   const service= {};
   service.running = false;
@@ -122,6 +137,84 @@ app.service('webSocketService', function ($http, $rootScope) {
     webSocket.onmessage = function (response) {
       service.aggregatedResponse += response.data
     };
+  };
+
+  return service;
+});
+
+app.service('chunkedXHRRequestService', function ($http,$q) {
+
+    var chunkedRequestWithPromise = function () {
+  var deferred = $q.defer();
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET",service.endpoint, true);
+  xhr.onprogress = function (data) {
+    deferred.notify(xhr.responseText);
+  };
+  xhr.onreadystatechange = function (oEvent) {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            deferred.resolve('success');
+        } else {
+            deferred.reject(xhr.statusText);
+        }
+    }
+  };
+  xhr.send();
+  return deferred.promise;
+};
+  const service= {};
+  service.running = false;
+  service.startTimestamp = undefined;
+  service.stopTimestamp = undefined;
+  service.transferred = 0;
+  service.endpoint = '/api/chunked';
+
+  service.start = function () {
+    console.log('starting request');
+    service.startTimestamp = Date.now();
+    service.running = true;
+   chunkedRequestWithPromise().then(response => {
+      service.stopTimestamp = Date.now();
+      service.running = false;
+      if (response.data) {
+      service.transferred = response.data.length;
+      console.log('request done');
+    }
+    }, null, response => {
+      service.stopTimestamp = Date.now();
+      if (response) {
+      service.transferred = response.length;
+    }
+    });
+  };
+  return service;
+});
+
+app.service('chunkedDecoratorRequestService', function ($http) {
+  const service= {};
+  service.running = false;
+  service.startTimestamp = undefined;
+  service.stopTimestamp = undefined;
+  service.transferred = 0;
+  service.endpoint = '/api/chunked';
+
+  service.start = function () {
+    console.log('starting request');
+    service.startTimestamp = Date.now();
+    service.running = true;
+    $http({method: "GET",
+        url: service.endpoint,
+        onProgress: function(event) {
+           service.transferred = event.target.response.length;
+           service.stopTimestamp = Date.now();
+           console.log('data', service.stopTimestamp -service.startTimestamp);
+        }}).then(response => {
+      service.stopTimestamp = Date.now();
+      service.running = false;
+      service.transferred = response.data.length;
+      console.log('request done');
+    });
   };
 
   return service;
